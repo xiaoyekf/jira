97f147410a8571dc186a41c951e4ec143d529a68
Wei Hong Bo <945135410@qq.com>
2022-11-14 17:49:43 +0800

    feat: 添加注册登录系统

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import { AuthenticatedApp } from 'authenticated-app';
+import { useAuth } from 'context/auth-context';
 import React from 'react';
+import { UnauthenticatedApp } from 'unauthenticated-app';
+
+import { LoginScreen } from 'unauthenticated-app/login';
 import './App.css';
-import { LoginScreen } from 'screens/login';
 
 function App() {
-    return (
-        <div className="App">
-            {/* <ProjectListScreen /> */}
-            <LoginScreen />
-        </div>
-    );
+    const { user } = useAuth();
+    return <div className="App">{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}</div>;
 }
 
 export default App;
--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,18 +1,16 @@
 import { useAuth } from 'context/auth-context';
 import React, { FormEvent } from 'react';
 
-export const LoginScreen = () => {
-    const { login, user } = useAuth();
+export const RegisterScreen = () => {
+    const { register, user } = useAuth();
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const username = (event.currentTarget.elements[0] as HTMLInputElement).value;
         const password = (event.currentTarget.elements[1] as HTMLInputElement).value;
-        login({ username, password });
+        register({ username, password });
     };
     return (
         <form onSubmit={handleSubmit}>
-            登录成功用户名{user?.name}
-            {user?.token}
             <div>
                 <label htmlFor="usename">用户名</label>
                 <input type="text" id={'usename'} />
@@ -21,7 +19,7 @@ export const LoginScreen = () => {
                 <label htmlFor="password">密码</label>
                 <input type="text" id={'password'} />
             </div>
-            <button type={'submit'}>登录</button>
+            <button type={'submit'}>注册</button>
         </form>
     );
 };
