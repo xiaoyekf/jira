4cae768136f8c831f0b00a67ae9c060ad4499912
Wei Hong Bo <945135410@qq.com>
2022-12-01 14:44:55 +0800

    feat: 阻止在以卸载组件上赋值

--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useMountedRef } from 'utils';
 
 interface State<D> {
     error: Error | null;
@@ -21,6 +22,7 @@ export const useAsync = <D>(initialState?: State<D>, initialConfig?: typeof defa
         ...defaultInitialState,
         ...initialState,
     });
+    const mountedRef = useMountedRef();
     const [retry, setRetry] = useState(() => () => {});
     const setData = (data: D) =>
         setState({
@@ -48,7 +50,7 @@ export const useAsync = <D>(initialState?: State<D>, initialConfig?: typeof defa
         setState({ ...state, stat: 'loading' });
         return promise
             .then((data) => {
-                setData(data);
+                if (mountedRef.current) setData(data);
                 return data;
             })
             .catch((error) => {
